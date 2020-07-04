05734f7fe1bb462ffa24314f14da09c3f63e6156
koduki <pascalm3+git@gmail.com>
2020-07-04 15:25:49 -0700

    Init other project

---
+++ slide4vr/pptx4vr-server/src/main/java/pptx4vr/UploadResource.java
@@ -0,0 +1,16 @@
+package pptx4vr;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/upload")
+public class UploadResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "hello";
+    }
+}
---
+++ slide4vr/pptx4vr-server/src/test/java/pptx4vr/NativeUploadResourceIT.java
@@ -0,0 +1,9 @@
+package pptx4vr;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeUploadResourceIT extends UploadResourceTest {
+
+    // Execute the same tests but in native mode.
+}
